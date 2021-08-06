5aae23ba6edbc7b6c333168a1353cce503ee2ab7
NishchayBhutani <bhutaninishchay9@gmail,com>
2021-08-06 22:56:14 +0530

    getting user input

--- app/src/main/java/com/nishchay/interactivitydemo/MainActivity.java
+++ app/src/main/java/com/nishchay/interactivitydemo/MainActivity.java
@@ -5,11 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     public void clickFunction(View view) {
+        EditText usernameEditText = (EditText) findViewById(R.id.usernameEditText);
+
+        EditText passwordEditText = (EditText) findViewById(R.id.passwordEditText);
+
         Log.i("info", "it worked!");
+        Log.i("Username", usernameEditText.getText().toString());
+        Log.i("Password", passwordEditText.getText().toString());
     }
 
     @Override
