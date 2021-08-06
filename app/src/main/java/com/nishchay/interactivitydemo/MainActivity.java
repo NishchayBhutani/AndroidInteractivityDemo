0f5e6b57a296cc63e7463ea435c82b59c72c8070
NishchayBhutani <bhutaninishchay9@gmail,com>
2021-08-06 20:32:35 +0530

    initial commit

---
+++ app/src/main/java/com/nishchay/interactivitydemo/MainActivity.java
@@ -0,0 +1,20 @@
+package com.nishchay.interactivitydemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void clickFunction(View view) {
+        Log.i("info", "it worked!");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
